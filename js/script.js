1e8817794c92d350381c44d371415c7d1597e2c4
beastboixdna <beastboigithub89@gmail.com>
2023-04-14 17:44:05 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var navLinker = document.getElementById('naver');
+var naver = document.getElementById('fa-bars');
+
+naver.addEventListener('click', function (e) {
+    navLinker.classList.toggle("open")
+})
